b37eb611f46a1e9892de3782ba819c4c1f69687d
Yuvankumar <93638740+yuvankumarL@users.noreply.github.com>
2024-11-13 09:25:40 +0000

    background removal

---
+++ final_v4/remove_background.py
@@ -0,0 +1,11 @@
+from rembg import remove
+import easygui
+from PIL import Image
+
+# inputPath = easygui.fileopenbox(title='select image file')
+# outputpath = easygui.fileopenbox(title='Save file to..')
+
+input = Image.open("final_v2/test/TomatoHealthy2.JPG")
+output = remove(input)
+output.save("final_v4/remove_background.png")
+
